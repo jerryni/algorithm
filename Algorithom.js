11c794e71921f6115e1113f670eb9201544538b7
hznirizhe <hznirizhe@corp.netease.com>
2017-05-23 16:06:05 +0800

    update rewrite htmlparser method

--- Algorithom.js
+++ Algorithom.js
@@ -1,5 +1,5 @@
 class Algorithom {
-    constructor(){}
+    constructor() {}
 
     /**
      * Ease calculator 字符串加减法 计算器
@@ -26,7 +26,7 @@ class Algorithom {
      * @param  {String} s
      * @return {Number}
      */
-    calculate(s){
+    calculate(s) {
         var i,
             c,
             sign = 1,
@@ -34,28 +34,28 @@ class Algorithom {
             number = 0,
             stack = []
 
-        if(!s || !s.length) {
+        if (!s || !s.length) {
             return 0
         }
 
-        for(i=0; i < s.length;i++){
+        for (i = 0; i < s.length; i++) {
             c = s[i];
 
-            if(/^[0-9]+$/.test(c)){
+            if (/^[0-9]+$/.test(c)) {
                 number = number * 10 + parseInt(c);
-            } else if(c == '+' || c== '-') {
+            } else if (c == '+' || c == '-') {
                 result += sign * number
                 sign = c == '+' ? 1 : -1
                 number = 0
-            } else if(c == '(') {
+            } else if (c == '(') {
                 stack.push(result)
                 stack.push(sign)
 
-                sign = 1              
+                sign = 1
                 number = 0
-                
+
                 result = 0 // the result in parenthesis
-            } else if(c == ')') {
+            } else if (c == ')') {
                 result += sign * number // result in parenthesis
                 number = 0
                 sign = 1
@@ -146,115 +146,123 @@ class Algorithom {
      */
     htmlAstParser(htmlStr) {
 
-        var regs = {
+        let ast = [],
+            regs = {
 
-            // 自闭合标签
-            selfCloseTag: /<(\w+)([^<]*)\/>/i,
+                // 自闭合标签
+                selfCloseTag: /<(\w+)([^<]*)\/>/i,
 
-            // 开标签
-            startTag: /(^[^<>]*)<(\w+)([^<]*)>/i,
+                // 开标签
+                startTag: /(^[^<>]*)<(\w+)([^<]*)>/i,
 
-            // 标签结尾
-            closeTag: /^([^<>]*)<\/(\w+)>/i
-        },
-        ast = []
+                // 标签结尾
+                closeTag: /^([^<>]*)<\/(\w+)>/i
+            }
 
         // 先写出整体的tag， 内容/属性先忽略
-        function parse(_ast){
-            var match = regs.startTag.exec(htmlStr)
+        function parse(_ast) {
+            let newAst,
+                node,
+                addedNode,
+                isSelfCloseTagMatch,
+                isCloseTagMatch,
+                match = regs.startTag.exec(htmlStr)
 
-            if (match) {
-                var macthedTotalStr = match[0],
-                    beforeStartTagTextNode = match[1] || '',
-                    tagName = match[2],
-                    newAst,
-                    node,
-                    newMatch,
-                    subMatch,
-                    attrs = []
+            if (!match) {
 
-                // 开标签前的文本内容
-                if(beforeStartTagTextNode){
-                    _ast.push({
-                        type: 'textNode',
-                        value: beforeStartTagTextNode
-                    })
-                }
-
-                // 是否是自闭合标签
-                if (subMatch = regs.selfCloseTag.exec(macthedTotalStr)) {
-                    attrs = subMatch[2].trim().split(' ');
-                    attrs = attrs.map(item => {
-                        return {
-                            attrName: item.split('=')[0],
-                            value: item.split('=')[1].replace(/"/g,'')
-                        }
-                    })
-
-                    _ast.push({
-                        tagName,
-                        attrs
-                    })
-
-                    // 截取掉匹配过的内容
-                    htmlStr = htmlStr.slice(match.index + macthedTotalStr.length)
-
-                    newAst = _ast // 用原来的节点
-
-                } else { //开标签 创建一个tagName, children
-                    
-                    // 添加属性
-                    if(match[3]) {
-                        attrs = match[3].trim().split(' ');
-
-                        attrs = attrs.map(item => {
-                            return {
-                                attrName: item.split('=')[0],
-                                value: item.split('=')[1].replace(/"/g,'')
-                            }
-                        })    
-                    }
-                    
-                    node = {
-                        tagName,
-                        attrs,
-                        children: []
-                    };
-
-                    // 截取掉匹配过的内容
-                    htmlStr = htmlStr.slice(match.index + macthedTotalStr.length)
-
-                    // 匹配到下一个是否是闭合标签，如果是就按照自闭合标签处理
-                    newMatch = regs.closeTag.exec(htmlStr)
-                    if(newMatch){
-
-                        // 标签名相同 说明是匹配的一对
-                        if(newMatch[2] == tagName){
-                            htmlStr = htmlStr.slice(newMatch.index + newMatch[0].length)
-                            if(newMatch[1]){
-                                node.children.push({
-                                    type: 'textNode',
-                                    value: newMatch[1]
-                                })
-                            }
-
-                            _ast.push(node)
-                            newAst = _ast // 用原来的节点
-                        }
-                    } else{
-
-                        _ast.push(node)
-                        newAst = node.children // 用新的节点
-                    }
-                }
-
-                // 进入递归
-                return parse(newAst)
-            } else {
+                // 解析完成
                 return ast
             }
+
+            let [macthedTotalStr, txtBeforeStartTag, tagName, attrs] = match
+
+            // 开标签前的文本内容
+            if (txtBeforeStartTag) {
+                _ast.push({
+                    type: 'textNode',
+                    value: txtBeforeStartTag
+                })
+            }
+
+            // 是否是自闭合标签
+            isSelfCloseTagMatch = regs.selfCloseTag.exec(macthedTotalStr)
+            if (isSelfCloseTagMatch) {
+                attrs = isSelfCloseTagMatch[2].trim().split(/\s+/)
+                attrs = attrs.map(item => {
+                    return {
+                        attrName: item.split('=')[0],
+                        value: item.split('=')[1].replace(/"/g, '')
+                    }
+                })
+
+                addedNode = {
+                    tagName,
+                    attrs
+                }
+
+                _ast.push(addedNode)
+
+                // 截取掉匹配过的内容
+                htmlStr = htmlStr.slice(match.index + macthedTotalStr.length)
+                newAst = _ast // 用原来的节点
+
+            } else { //开标签的情况： 创建一个tagName, children
+
+                // 添加属性
+                if (attrs) {
+                    attrs = attrs.trim().split(' ').map(item => {
+                        return {
+                            attrName: item.split('=')[0],
+                            value: item.split('=')[1].replace(/"/g, '')
+                        }
+                    })
+                }
+
+                node = {
+                    tagName,
+                    attrs,
+                    children: []
+                }
+
+                // 截取掉匹配过的内容
+                htmlStr = htmlStr.slice(match.index + macthedTotalStr.length)
+
+                // 匹配到下一个是否是闭合标签，如果是就按照自闭合标签处理
+                isCloseTagMatch = regs.closeTag.exec(htmlStr)
+                if (isCloseTagMatch) {
+
+                    let [matchedStr, textNodeStr, tagName2] = isCloseTagMatch
+
+                    // 标签名相同 说明是匹配的一对
+                    if (tagName2 == tagName) {
+
+                        // 截取掉匹配过的内容
+                        htmlStr = htmlStr.slice(isCloseTagMatch.index + matchedStr.length)
+
+                        // 匹配到的文本内容添加到子节点里
+                        if (textNodeStr) {
+                            node.children.push({
+                                type: 'textNode',
+                                value: textNodeStr
+                            })
+                        }
+                    }
+
+
+                    _ast.push(node)
+                    newAst = _ast // 用原来的节点
+
+                } else {
+
+                    _ast.push(node)
+                    newAst = node.children // 用新的子节点
+                }
+            }
+
+            // 一轮解析完成之后，传入新的ast节点，再进行解析
+            return parse(newAst)
         }
 
-        return parse(ast);
+        return parse(ast)
     }
 }
